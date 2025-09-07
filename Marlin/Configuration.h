6d93f66d199d507bef1b44c4fef0572377036c50
JustinMa <JustinMa@mail.com>
2025-09-07 18:15:24 -0400

    set z prob offset to -1.80 as default and 7X7 for prob

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1511,7 +1511,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -27, 10, -3.0 }
+#define NOZZLE_TO_PROBE_OFFSET { -27, 10, -1.80 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1991,7 +1991,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 7
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
